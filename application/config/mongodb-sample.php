8ff94f7cff9bbbf5074f526f1006cb4f9f55ed37
Thanakij Pechprasarn <thanakij.p@foremost-ai.com>
2024-10-16 17:18:17 +0700

    fix: MongoCollection::insert(): The 'safe' option is deprecated, use 'w' instead

--- application/config/mongodb-sample.php
+++ application/config/mongodb-sample.php
@@ -23,7 +23,7 @@ $config['default']['mongo_password'] = '';
 $config['default']['mongo_persist']  = FALSE;
 $config['default']['mongo_persist_key']	 = 'ci_persist';
 $config['default']['mongo_replica_set']  = FALSE;
-$config['default']['mongo_query_safety'] = 'safe';
+$config['default']['mongo_query_safety'] = 'w';
 $config['default']['mongo_suppress_connect_error'] = TRUE;
 $config['default']['mongo_host_db_flag']   = FALSE;
 
